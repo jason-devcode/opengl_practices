1ae22dddcafc3ca025fdcd229ff507076d7a0ca2
jason-devcode <80931913+jason-devcode@users.noreply.github.com>
2026-03-01 15:51:31 -0500

    feat: added input sample

---
+++ practices/02_input/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int width = 512;
+int height = 512;
+
+void processInput( GLFWwindow* window ) {
+    if( glfwGetKey( window, GLFW_KEY_ESCAPE ) == GLFW_PRESS ) {
+        glfwSetWindowShouldClose( window, true );
+    }
+}
+
+int main() {
+    glfwInit();
+
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+    glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+
+    GLFWwindow* window = glfwCreateWindow( width, height, "02 - Input Example", NULL, NULL );
+
+    if( window == NULL ) {
+        std::cerr << "Failed to create window!\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent( window );
+
+    if( not gladLoadGLLoader( (GLADloadproc) glfwGetProcAddress ) ) {
+        std::cerr << "Failed to load GL loader!\n";
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return -1;
+
+    }
+
+    glViewport(0,0,width,height);
+    glClearColor(0,0,1,1);
+
+    while (not glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        processInput(window);
+
+        glClear( GL_COLOR_BUFFER_BIT );
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
+
