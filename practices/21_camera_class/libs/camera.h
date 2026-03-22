3f71af2270e3163d443537a0b21c8215bb3ca726
jason-devcode <80931913+jason-devcode@users.noreply.github.com>
2026-03-21 19:57:15 -0500

    feat: added camera class

---
+++ practices/21_camera_class/libs/camera.h
@@ -0,0 +1,61 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+using vec2 = glm::vec2;
+using vec3 = glm::vec3;
+using vec4 = glm::vec4;
+
+using mat4 = glm::mat4;
+
+class Camera {
+    public:
+        Camera(){};
+
+        void move_forward( float sign ) {
+            position += direction * sign;
+        }
+
+        void rotate( vec3 delta_rotation ) {
+            rotation += delta_rotation;
+        }
+
+        void update() {
+            mat4 rot = mat4(1.0f);
+
+            rot = glm::rotate( rot, rotation.z, vec3(0.0f, 0.0f, 1.0f) ); // Roll
+            rot = glm::rotate( rot, rotation.y, vec3(0.0f, 1.0f, 0.0f) ); // Yaw
+            rot = glm::rotate( rot, rotation.x, vec3(1.0f, 0.0f, 0.0f) ); // Pitch
+
+            direction = glm::normalize( vec3( rot * vec4(0.0f,  0.0f, -1.0f, 0.0f) ) );
+            right     = glm::normalize( vec3( rot * vec4(1.0f,  0.0f,  0.0f, 0.0f) ) );
+            up        = glm::normalize( vec3( rot * vec4(0.0f,  1.0f,  0.0f, 0.0f) ) );
+        }
+
+        mat4 get_lookup_view() {
+            return glm::lookAt( position, position + direction, up );
+        }
+
+
+        void print_info() {
+            std::cout << "--------------------------------";
+            std::cout << "Camera pos "   << "X: " << position.x << ", "  << "Y: " << position.y << ", " << "Z: " << position.z << "\n"; 
+            std::cout << "Camera dir "   << "X: " << direction.x << ", "  << "Y: " << direction.y << ", " << "Z: " << direction.z << "\n"; 
+            std::cout << "Camera right " << "X: " << right.x << ", "  << "Y: " << right.y << ", " << "Z: " << right.z << "\n"; 
+            std::cout << "Camera up " << "X: " << up.x << ", "  << "Y: " << up.y << ", " << "Z: " << up.z << "\n"; 
+            std::cout << "Camera rotation " << "X: " << rotation.x << ", "  << "Y: " << rotation.y << ", " << "Z: " << rotation.z << "\n"; 
+        }
+    public:
+        vec3 position  = vec3(0.0f, 0.0f, 0.0f);
+        vec3 rotation  = vec3(0.0f, 0.0f, 0.0f);    
+        vec3 direction = vec3(0.0f, 0.0f, -1.0f);
+        vec3 up        = vec3(0.0f, 0.0f, 0.0f);
+        vec3 right     = vec3(0.0f, 0.0f, 0.0f);
+};
+
+#endif
